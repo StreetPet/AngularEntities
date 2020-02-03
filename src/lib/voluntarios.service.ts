2299843b1c4fc08364ee1a6f4a828b4f55815dcc
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-02-03 02:32:31 -0300

    Criado bibliocas Menu, Entities e Auth
    Foram criadas as Bibliotecas Menu, Entities e Auth para separar e
    reaproveitar os códigos de cada recurso e funcionalidade de forma
    separadamente, permitindo a reutilização em outros projetos e outras
    aplicações do sistema.

--- src/lib/voluntarios.service.ts
+++ src/lib/voluntarios.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentChangeAction, DocumentSnapshot, Action } from '@angular/fire/firestore';
 import { Voluntario, AvatarVoluntario } from './voluntarios';
 import { Observable, Subscription } from 'rxjs';
 
@@ -18,56 +18,55 @@ export class VoluntariosService {
     return this.db.collection<AvatarVoluntario>('/avatars');
   }
 
-  getVoluntario(uid) {
+  getVoluntario(uid: string): Observable<Action<DocumentSnapshot<Voluntario>>> {
     return this.getCollectionVoluntarios().doc<Voluntario>(uid).snapshotChanges();
   }
 
-  updateVoluntario(uid: string, voluntario: Voluntario) {
+  updateVoluntario(uid: string, voluntario: Voluntario): Promise<void> {
     voluntario.nameToSearch = voluntario.nome.toLowerCase();
     return this.getCollectionVoluntarios().doc<Voluntario>(uid).set(voluntario);
   }
 
-  deleteVoluntario(uid: string) {
-      const subscription:Subscription = this.getCollectionVoluntarios()
+  deleteVoluntario(uid: string): Promise<void>{
+    const subscription: Subscription = this.getCollectionVoluntarios()
       .doc<Voluntario>(uid).valueChanges()
       .subscribe((voluntario: Voluntario) => {
         this.db.collection('/voluntarios_removidos').doc<Voluntario>(uid).set(voluntario);
         subscription.unsubscribe();
       });
-      return this.getCollectionVoluntarios().doc<Voluntario>(uid).delete();
+    return this.getCollectionVoluntarios().doc<Voluntario>(uid).delete();
   }
 
-  getVoluntarios(): Observable<any> {
+  getVoluntarios(): Observable<DocumentChangeAction<Voluntario>[]> {
     return this.getCollectionVoluntarios().snapshotChanges();
   }
 
-  private getCollectionVoluntarios(query = null) {
+  private getCollectionVoluntarios(query = null): AngularFirestoreCollection<Voluntario> {
     if (query)
       return this.db.collection<Voluntario>('/voluntarios', query);
     else
       return this.db.collection<Voluntario>('/voluntarios');
   }
 
-  searchVoluntario(searchValue: string) {
+  searchVoluntario(searchValue: string):Observable<DocumentChangeAction<Voluntario>[]> {
     return this.getCollectionVoluntarios(ref => ref.where('nameToSearch', '>=', searchValue)
       .where('nameToSearch', '<=', searchValue + '\uf8ff'))
       .snapshotChanges();
   }
 
-  searchVoluntarioPelaIdade(idade: number) {
-    const collection = this.db
-      .collection('voluntarios', ref => ref.orderBy('idade').startAt(idade));
+  searchVoluntarioPelaIdade(idade: number):Observable<DocumentChangeAction<Voluntario>[]> {
+    const collection = this.getCollectionVoluntarios(ref => ref.orderBy('idade').startAt(idade));
     return collection.snapshotChanges();
   }
 
   createVoluntario(voluntario: Voluntario, avatar: AvatarVoluntario) {
-    return this.db.collection('voluntarios')
-      .add({
+    return this.getCollectionVoluntarios()
+      .add(<Voluntario><unknown>{
         nome: voluntario.nome,
         nameToSearch: voluntario.nome.toLowerCase(),
         sobrenome: voluntario.sobrenome,
         idade: voluntario.idade,
-        avatar
+        avatar: avatar.link
       });
   }
 }
